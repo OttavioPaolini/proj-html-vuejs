bd751393945b5a5f67f153f810d17b389b9a7098
Ottavio8 <paolini.ottavio8@gmail.com>
2022-11-17 15:16:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import {store} from './store'
+
+
+export default {
+    data(){
+      return{
+        store,
+      }
+    },
+    
+
+}
+</script>
+
+<template>
+
+  <h1>Tutto collegato </h1>
+  <h2>{{store.test}}</h2>
+</template>
+
+<style lang="scss">
+@use "./assets/style/general.scss" as *;
+
+h1{
+  color: red;
+}
+</style>
